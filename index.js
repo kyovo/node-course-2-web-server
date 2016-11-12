e447f9c62bdae9207ad3950e36e37df8802ceeb2
Kyovo Digaw <kyovodigaw@gmail.com>
2016-11-12 22:47:05 +0100

    Setup start script and heroku port

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require('express'); //importamos los modulos
 const hbs = require('hbs');
 const fs = require('fs');
 
+//Con esto conseguimos que funcione Heroku
+//Ademas debemos indicar en el json "start": "node index.js" en scripts
+const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials'); //Los partials serian los layouts
+app.set('view engine', 'hbs'); //Indicamos que vamos a mostrar los datos con handelbars
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next)=> {
@@ -20,13 +23,17 @@ app.use((req, res, next)=> {
   next();
 });
 
-hbs.registerHelper('getCurrentYear', () => {
+//-- Creacion de los helpers que usaremos con {{> NombreHelper}}
+
+hbs.registerHelper('getCurrentYear', () => { // {{>getCurrentYear}}
   return new Date().getFullYear()
 });
 
-hbs.registerHelper('screamIt', (text) => {
+hbs.registerHelper('screamIt', (text) => { // {{>screamIt welcomeMessage}}
   return text.toUpperCase();
 });
+
+//Renderizamos las paginas de nuestra web
 
 app.get('/', (req, res) =>{
   res.render('home.hbs', {
@@ -42,11 +49,14 @@ app.get('/about', (req, res) => {
   });
 });
 
-//bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
   });
 });
 
-app.listen(3000);
+//El puerto que va a estar escuchando nuestra web
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
